67ff384ef5df14ff9fd198e0f78e92af7ddbb6e3
AbuBreak <94977176+AbuBreak@users.noreply.github.com>
2022-09-01 11:50:47 +0300

    RememberMe commit

--- app/src/main/java/com/example/movietimeapp/activity/LoginActivity.java
+++ app/src/main/java/com/example/movietimeapp/activity/LoginActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,13 +31,18 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
-    ImageView img_back;
-    EditText edit_email;
-    TextInputEditText edit_pass;
-    TextView txtForget, txtRegister;
-    Button btnLogin;
+    private static final String CHECKED = "rememberMe";
+    private ImageView img_back;
+    private EditText edit_email;
+    private TextInputEditText edit_pass;
+    private TextView txtForget, txtRegister;
+    private Button btnLogin;
+    private CheckBox cbRemember;
 
     private FirebaseAuth mAuth;
+    private DatabaseReference reference;
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +51,18 @@ public class LoginActivity extends AppCompatActivity {
 
         initViews();
         mAuth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+
+        sharedPreferences = getSharedPreferences(CHECKED, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        String saveEmail = sharedPreferences.getString("svEmail", "");
+        String savePass = sharedPreferences.getString("svPass", "");
+
+        cbRemember.setChecked(sharedPreferences.contains("checked") && sharedPreferences.getBoolean("checked", false));
+
+        edit_email.setText(saveEmail);
+        edit_pass.setText(savePass);
 
         img_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,44 +75,87 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (cbRemember.isChecked()) {
+                    editor.putBoolean("checked", true);
+                    editor.apply();
+                    StoreDataUsingPref(edit_email.getText().toString(), edit_pass.getText().toString());
 
-                if (edit_email.getText().toString().isEmpty()) {
-                    edit_email.setError("Email is required!");
-                    edit_email.requestFocus();
+                    if (edit_email.getText().toString().isEmpty()) {
+                        edit_email.setError("Email is required!");
+                        edit_email.requestFocus();
 
-                } else if (!Patterns.EMAIL_ADDRESS.matcher(edit_email.getText().toString()).matches()) {
-                    edit_email.setError("Provide a valid email!");
-                    edit_email.requestFocus();
+                    } else if (!Patterns.EMAIL_ADDRESS.matcher(edit_email.getText().toString()).matches()) {
+                        edit_email.setError("Provide a valid email!");
+                        edit_email.requestFocus();
 
-                } else if (edit_pass.getText().toString().isEmpty()) {
-                    edit_pass.setError("Password is required!");
-                    edit_pass.requestFocus();
+                    } else if (edit_pass.getText().toString().isEmpty()) {
+                        edit_pass.setError("Password is required!");
+                        edit_pass.requestFocus();
 
-                } else if (edit_pass.length() < 6) {
-                    edit_pass.setError("Min password length is 6 characters!");
-                    edit_pass.requestFocus();
+                    } else if (edit_pass.length() < 6) {
+                        edit_pass.setError("Min password length is 6 characters!");
+                        edit_pass.requestFocus();
 
-                }
-                mAuth.signInWithEmailAndPassword(edit_email.getText().toString(), edit_pass.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    }
+                    mAuth.signInWithEmailAndPassword(edit_email.getText().toString(), edit_pass.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-                                    if (user.isEmailVerified()) {
-                                        Intent intent1 = new Intent(LoginActivity.this, HomePageActivity.class);
-                                        startActivity(intent1);
+                                        if (user.isEmailVerified()) {
+                                            Intent intent1 = new Intent(LoginActivity.this, HomePageActivity.class);
+                                            startActivity(intent1);
+                                        } else {
+                                            user.sendEmailVerification();
+                                            Toast.makeText(LoginActivity.this, "Please check your email to verify your account!", Toast.LENGTH_SHORT).show();
+                                        }
                                     } else {
-                                        user.sendEmailVerification();
-                                        Toast.makeText(LoginActivity.this, "Please check your email to verify your account!", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(LoginActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_SHORT).show();
                                     }
-                                } else {
-                                    Toast.makeText(LoginActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_SHORT).show();
                                 }
-                            }
-                        });
+                            });
 
+                } else {
+                    if (edit_email.getText().toString().isEmpty()) {
+                        edit_email.setError("Email is required!");
+                        edit_email.requestFocus();
+
+                    } else if (!Patterns.EMAIL_ADDRESS.matcher(edit_email.getText().toString()).matches()) {
+                        edit_email.setError("Provide a valid email!");
+                        edit_email.requestFocus();
+
+                    } else if (edit_pass.getText().toString().isEmpty()) {
+                        edit_pass.setError("Password is required!");
+                        edit_pass.requestFocus();
+
+                    } else if (edit_pass.length() < 6) {
+                        edit_pass.setError("Min password length is 6 characters!");
+                        edit_pass.requestFocus();
+
+                    }
+                    getSharedPreferences(CHECKED,MODE_PRIVATE).edit().clear().commit();
+                    mAuth.signInWithEmailAndPassword(edit_email.getText().toString(), edit_pass.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                                        if (user.isEmailVerified()) {
+                                            Intent intent1 = new Intent(LoginActivity.this, HomePageActivity.class);
+                                            startActivity(intent1);
+                                        } else {
+                                            user.sendEmailVerification();
+                                            Toast.makeText(LoginActivity.this, "Please check your email to verify your account!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
             }
         });
         txtForget.setOnClickListener(new View.OnClickListener() {
@@ -114,6 +176,13 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    private void StoreDataUsingPref(String email, String pass) {
+        SharedPreferences.Editor editor = getSharedPreferences(CHECKED, MODE_PRIVATE).edit();
+        editor.putString("svEmail", email);
+        editor.putString("svPass", pass);
+        editor.apply();
+    }
+
     private void initViews() {
         img_back = findViewById(R.id.img_back);
         edit_email = findViewById(R.id.confrimPass);
@@ -121,5 +190,6 @@ public class LoginActivity extends AppCompatActivity {
         txtForget = findViewById(R.id.txtForget);
         txtRegister = findViewById(R.id.txtRegister);
         btnLogin = findViewById(R.id.loginBtn);
+        cbRemember = findViewById(R.id.cbRemember);
     }
 }
